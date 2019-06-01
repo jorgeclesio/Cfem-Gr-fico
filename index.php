c080ff414d5ad483064b27d4ea1ad8232bedf51b
Jorge Clésio <jorgepebas@gmail.com>
2019-06-01 02:27:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
   	$conexao = mysqli_connect ("host","user","senha",'bd') or die ("Err SERVER");
 	date_default_timezone_set('America/Belem');
 	?>
-
+	<!-- Formulário -->
 	<center>
-	<form>
+	<form method="GET" action="">
 	<select id="ano" name="cidade">
 	<?php
 		if(!$_GET){
@@ -43,35 +43,30 @@
 	
 	<button>Processar</button>
 
-	</form>	
+	</form>	 	<!-- FIM Formulário -->
 	</center>
 
-  
- <script type="text/javascript" src="http://geocity.hol.es/cfem/loader.js"></script>
-  <script type="text/javascript">
+ <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
 <?php 
-$cor = array('#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF');
-?>		  
-		  
-
-	
+		$cor = array('#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+				  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+				  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+				  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+				  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+				  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+				  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+				  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+				  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+				  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF');
+		?>		  
     google.charts.load("current", {packages:['corechart']});
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
       var data = google.visualization.arrayToDataTable([
         ["Mes", "Valor", { role: "style" }],
-		
 		<?php 
-		
+
 		$ano=$_GET['ano'];
 		$cidade=$_GET['cidade'];
 		
@@ -106,7 +101,8 @@ $cor = array('#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
       chart.draw(view, options);
   }
   </script>
-  <div style="text-align:center;">
+												   
+  	<div style="text-align:center;"> <!-- Valor Total -->
 	<?php 
 		$ano=$_GET['ano'];
 		$cidade=$_GET['cidade'];
@@ -117,7 +113,8 @@ $cor = array('#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
 		<?php echo "Total:<b> R$". number_format($dado['valor']) ."</b>";?>
 		<?php } ?>
 		
-	</div>
+	</div> <!-- FIM Valor Total -->
+				       
 <?php if($_GET){?>
 <div id="columnchart_values" style="width: 110%; height: 300px;"></div>
 <?php } ?>
